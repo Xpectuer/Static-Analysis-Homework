82324350346ba64e2735f91e5d06ef305d8df2a1
Noobi <xpectuer223@gmail.com>
2022-01-22 17:35:09 +0800

    init repo

--- src/main/java/org/xpectuer/Main.java
+++ src/main/java/org/xpectuer/Main.java
@@ -1,5 +1,11 @@
 package org.xpectuer;
 
+// Go to see the tests
+
+/**
+ * launcher is written in the tests
+ * go to test dir and find them !!!
+ */
 public class Main {
     public static void main(String[] args) {
 
