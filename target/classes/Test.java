9a15cab9a742d0ce7e7d633e695d972b734a5898
Xpectuer <xpectuer223@gmail.com>
2022-02-15 16:35:49 +0800

    debugged: undefined variable throws null ptr exception.

---
+++ target/classes/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = x;
+        int a, b, c, d;
+
+        a = x + 2;
+        b = y + 3;
+        c = x + y <<  1;
+        d = a + b + c;
+
+        System.out.println(d);
+
+        return;
+    }
+}
